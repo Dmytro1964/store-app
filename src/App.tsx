40a70ef7046547ccd0c5c2fc76e20b88ba30e74b
Alisher Khamidov <a.a.khamidov@yandex.ru>
2026-03-10 13:08:23 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Greeting from "./components/Greeting/Greeting";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Greeting name="John" />
+      <Greeting name="Bob" />
+      <Greeting name="Ioann" age={20} />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
+
+// Hero - внутри section
+// Buy our unique product!
+
+// SPA single page application
